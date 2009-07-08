a89e4eb6081c703f687523e428f28cbde0a0c219
Matt Courtney <mcourtne199@yahoo.com>
2009-07-08 10:44:32 -0400

    Added a test for controller/builder.clj

--- file_structure/default/vendor/conjure/controller/builder.clj
+++ file_structure/default/vendor/conjure/controller/builder.clj
@@ -4,11 +4,14 @@
 
 (defn
 #^{:doc "Creates a new controller file from the given controller name."}
-  create-controller-file [controllers-directory controller-name]
-  (let [controller-file (new File controllers-directory (util/controller-file-name-string controller-name))]
-    (if (. controller-file exists)
-      (println (. controller-file getName) "already exits. Doing nothing.")
-      (do
-        (println "Creating controller file" (. controller-file getName) "...")
-        (. controller-file createNewFile)
-        controller-file))))
+  create-controller-file 
+  ([controller-name] (create-controller-file (util/find-controllers-directory) controller-name))
+  ([controllers-directory controller-name]
+    (if (and controllers-directory controller-name)
+      (let [controller-file (new File controllers-directory (util/controller-file-name-string controller-name))]
+        (if (. controller-file exists)
+          (println (. controller-file getName) "already exits. Doing nothing.")
+          (do
+            (println "Creating controller file" (. controller-file getName) "...")
+            (. controller-file createNewFile)
+            controller-file))))))
---
+++ test/controller/test_builder.clj
@@ -0,0 +1,18 @@
+(ns test.controller.test-builder
+  (:use test-helper
+        clojure.contrib.test-is
+        conjure.controller.builder)
+  (:require [conjure.controller.util :as util]))
+        
+(deftest test-create-controller-file
+  (let [controller-directory (util/find-controllers-directory)
+        controller-file (create-controller-file controller-directory "test")]
+    (test-file controller-file "test_controller.clj")
+    (. controller-file delete))
+  (let [controller-file (create-controller-file "test")]
+    (test-file controller-file "test_controller.clj")
+    (. controller-file delete))
+  (is (nil? (create-controller-file (util/find-controllers-directory) nil)))
+  (is (nil? (create-controller-file nil "test")))
+  (is (nil? (create-controller-file nil nil)))
+  (is (nil? (create-controller-file nil))))
