616489fe714f2ea2ca41ee2e067e7547eeedec58
Matt Courtney <mcourtne199@yahoo.com>
2009-07-09 17:00:52 -0400

    Reduced the amount of text sent to standard out when running the tests.

--- file_structure/default/vendor/conjure/migration/runner.clj
+++ file_structure/default/vendor/conjure/migration/runner.clj
@@ -3,28 +3,35 @@
   (:require [conjure.migration.util :as util]
             [conjure.model.database :as database]))
 
+(defn
+#^{:doc "Prints the given args to standard out if quiet evaluates to true. Otherwise, do nothing."}
+  quiet-println [quiet & args]
+  (if (not quiet) (apply println args)))
+
 (defn 
 #^{:doc "Gets the current db version number. If the schema info table doesn't exist this function creates it. If the 
 schema info table is empty, then it adds a row and sets the version to 0."}
-  current-db-version []
+  current-db-version
+  ([] (current-db-version false))
+  ([quiet]
     (if (database/table-exists? util/schema-info-table)
       (do
-        (println util/schema-info-table "exists")
+        (quiet-println quiet util/schema-info-table "exists")
         (let [version-results (database/sql-find { :table util/schema-info-table :limit 1 })
               version-result-map (first version-results)]
           (if version-result-map
             (get version-result-map util/version-column)
             (do
-              (println util/schema-info-table "is empty.")
+              (if (not quiet) (println util/schema-info-table "is empty."))
               (database/insert-into util/schema-info-table { util/version-column 0 })
               0))))
       (do
-        (println util/schema-info-table "does not exist. Creating table...")
+        (quiet-println quiet  util/schema-info-table "does not exist. Creating table...")
         (database/create-table util/schema-info-table 
           (database/integer "version" { :not-null true }))
-        (println "Setting the initial version to 0.")
+        (quiet-println quiet  "Setting the initial version to 0.")
         (database/insert-into util/schema-info-table {util/version-column 0})
-        0)))
+        0))))
 
 (defn
 #^{:doc "Updates the version number saved in the schema table in the database."}
@@ -33,86 +40,98 @@ schema info table is empty, then it adds a row and sets the version to 0."}
 
 (defn
 #^{:doc "Runs the up function in the given migration file."}
-  run-migrate-up [migration-file]
-  (if (and migration-file (instance? File migration-file))
-    (do
-      (println "Running" (. migration-file getName) "up...")
-      (load-file (. migration-file getAbsolutePath))
-      (load-string (str "(" (util/migration-namespace migration-file) "/up)"))
-      (let [new-version (util/migration-number-from-file migration-file)]
-        (update-db-version new-version)
-        new-version))
-    (println "Invalid migration-file:" migration-file "No changes were made to the database.")))
+  run-migrate-up 
+  ([migration-file] (run-migrate-up migration-file false))
+  ([migration-file quiet]
+    (if (and migration-file (instance? File migration-file))
+      (do
+        (quiet-println quiet "Running" (. migration-file getName) "up...")
+        (load-file (. migration-file getAbsolutePath))
+        (load-string (str "(" (util/migration-namespace migration-file) "/up)"))
+        (let [new-version (util/migration-number-from-file migration-file)]
+          (update-db-version new-version)
+          new-version))
+      (quiet-println quiet "Invalid migration-file:" migration-file "No changes were made to the database."))))
   
 (defn
 #^{:doc "Runs the down function in the given migration file."}
-  run-migrate-down [migration-file]
-  (if (and migration-file (instance? File migration-file))
-    (do
-      (println "Running" (. migration-file getName) "down...")
-      (load-file (. migration-file getAbsolutePath))
-      (load-string (str "(" (util/migration-namespace migration-file) "/down)"))
-      (let [new-version (util/migration-number-before (util/migration-number-from-file migration-file))]
-        (update-db-version new-version)
-        new-version))
-    (println "Invalid migration-file:" migration-file "No changes were made to the database.")))
+  run-migrate-down 
+  ([migration-file] (run-migrate-down migration-file false))
+  ([migration-file quiet]
+    (if (and migration-file (instance? File migration-file))
+      (do
+        (quiet-println quiet "Running" (. migration-file getName) "down...")
+        (load-file (. migration-file getAbsolutePath))
+        (load-string (str "(" (util/migration-namespace migration-file) "/down)"))
+        (let [new-version (util/migration-number-before (util/migration-number-from-file migration-file))]
+          (update-db-version new-version)
+          new-version))
+      (quiet-println quiet "Invalid migration-file:" migration-file "No changes were made to the database."))))
         
 (defn
 #^{:doc "Runs the up function on all of the given migration files."}
   migrate-up-all
-  ([] (migrate-up-all (util/all-migration-files)))
-  ([migration-files]
+  ([] (migrate-up-all (util/all-migration-files) false))
+  ([migration-files] (migrate-up-all migration-files false))
+  ([migration-files quiet]
     (loop [other-migrations migration-files
            output nil]
       (if (not-empty other-migrations)
         (recur
           (rest other-migrations)
-          (run-migrate-up (first other-migrations)))
+          (run-migrate-up (first other-migrations) quiet))
         output))))
 
 (defn
 #^{:doc "Runs the up function on all of the given migration files."}
   migrate-down-all
-  ([] (migrate-down-all (reverse (util/all-migration-files))))
-  ([migration-files]
+  ([] (migrate-down-all (reverse (util/all-migration-files)) false))
+  ([migration-files] (migrate-down-all migration-files false))
+  ([migration-files quiet]
     (loop [other-migrations migration-files
            output nil]
       (if (not-empty other-migrations)
         (recur
           (rest other-migrations)
-          (run-migrate-down (first other-migrations)))
+          (run-migrate-down (first other-migrations) quiet))
         output))))
 
 (defn
 #^{:doc "Migrates the database up from from-version to to-version."}
-  migrate-up [from-version to-version]
-  (if (and from-version to-version)
-    (let [new-version (migrate-up-all (util/migration-files-in-range from-version to-version))]
-      (if new-version
-        (println "Migrated to version:" new-version)
-        (println "No changes were made to the database.")))
-    (println "Invalid version number:" from-version "or" to-version "No changes were made to the database.")))
+  migrate-up
+  ([from-version to-version] (migrate-up from-version to-version false))
+  ([from-version to-version quiet]
+    (if (and from-version to-version)
+      (let [new-version (migrate-up-all (util/migration-files-in-range from-version to-version) quiet)]
+        (if new-version
+          (quiet-println quiet "Migrated to version:" new-version)
+          (quiet-println quiet "No changes were made to the database.")))
+      (quiet-println quiet "Invalid version number:" from-version "or" to-version "No changes were made to the database."))))
   
 (defn
 #^{:doc "Migrates the database down from from-version to to-version."}
-  migrate-down [from-version to-version]
-  (if (and from-version to-version)
-    (let [new-version (migrate-down-all (reverse (util/migration-files-in-range to-version from-version)))]
-      (if new-version
-        (println "Migrated to version:" new-version)
-        (println "No changes were made to the database.")))
-    (println "Invalid version number:" from-version "or" to-version "No changes were made to the database.")))
+  migrate-down
+  ([from-version to-version] (migrate-down from-version to-version false))
+  ([from-version to-version quiet]
+    (if (and from-version to-version)
+      (let [new-version (migrate-down-all (reverse (util/migration-files-in-range to-version from-version)) quiet)]
+        (if new-version
+          (quiet-println quiet "Migrated to version:" new-version)
+          (quiet-println quiet "No changes were made to the database.")))
+      (quiet-println quiet "Invalid version number:" from-version "or" to-version "No changes were made to the database."))))
 
 (defn 
 #^{:doc "Updates the database to the given version number. If the version number is less than the current database 
 version number, then this function causes a roll back."}
-  update-to-version [version-number]
-  (if version-number
-    (let [db-version (current-db-version)]
-      (println "Current database version:" (str db-version))
-      (let [version-number-min (min (max version-number 0) (util/max-migration-number (util/find-migrate-directory)))]
-        (println "Updating to version:" version-number-min)
-        (if (< db-version version-number-min)
-          (migrate-up (+ db-version 1) version-number-min)
-          (migrate-down db-version (+ version-number-min 1)))))
-    (println "Invalid version-number:" version-number)))
+  update-to-version
+  ([version-number] (update-to-version version-number false))
+  ([version-number quiet]
+    (if version-number
+      (let [db-version (current-db-version quiet)]
+        (quiet-println quiet "Current database version:" (str db-version))
+        (let [version-number-min (min (max version-number 0) (util/max-migration-number (util/find-migrate-directory)))]
+          (quiet-println quiet "Updating to version:" version-number-min)
+          (if (< db-version version-number-min)
+            (migrate-up (+ db-version 1) version-number-min quiet)
+            (migrate-down db-version (+ version-number-min 1) quiet))))
+      (quiet-println quiet "Invalid version-number:" version-number))))
