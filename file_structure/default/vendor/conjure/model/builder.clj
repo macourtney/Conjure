15b60c03a6185988182b1e36041aea511c5a39f5
Matt Courtney <mcourtne199@yahoo.com>
2009-07-10 09:42:58 -0400

    Added tests for model/builder.clj and model/database.clj.

--- file_structure/default/vendor/conjure/model/builder.clj
+++ file_structure/default/vendor/conjure/model/builder.clj
@@ -4,11 +4,16 @@
 
 (defn
 #^{:doc "Creates a new model file from the given model name."}
-  create-model-file [models-directory model-name]
-  (let [model-file (new File models-directory (util/model-file-name-string model-name))]
-    (if (. model-file exists)
-      (println (. model-file getName) "already exits. Doing nothing.")
-      (do
-        (println "Creating model file" (. model-file getName) "...")
-        (. model-file createNewFile)
-        model-file))))
+  create-model-file
+  ([model-name] (create-model-file (util/find-models-directory) model-name))
+  ([models-directory model-name]
+    (if (and models-directory model-name)
+      (let [model-file (new File models-directory (util/model-file-name-string model-name))]
+        (if (. model-file exists)
+          (do
+            (println (. model-file getName) "already exits. Doing nothing.")
+            model-file)
+          (do
+            (println "Creating model file" (. model-file getName) "...")
+            (. model-file createNewFile)
+            model-file))))))
