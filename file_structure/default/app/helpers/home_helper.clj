0a21d9ab5cc1395b2bf0e16926ae730f4512c1b6
Matthew Courtney <mcourtne199@yahoo.com>
2010-02-01 22:13:31 -0500

    Actually added the home helper this time.

---
+++ file_structure/default/app/helpers/home_helper.clj
@@ -0,0 +1,10 @@
+(ns helpers.home-helper)
+
+(defn
+#^{ :doc "Creates the home links and adds them to the layout info in request-map." }
+  home-request-map [request-map]
+  (assoc 
+      request-map 
+      :layout-info 
+      { :links 
+        [{ :text "Home", :url-for { :controller "home", :action "index" } }]}))
