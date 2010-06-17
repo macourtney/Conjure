38a17f0c68a469b9e6f14ab65fa289ec777705f6
Matt Courtney <macourtney@gmail.com>
2010-06-17 17:34:39 -0400

    Updated the description in the conjure project file.

--- conjure/project.clj
+++ conjure/project.clj
@@ -1,5 +1,5 @@
 (defproject conjure "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Conjure is a full stack web framework written entirely in Clojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [conjure-script "0.7.0-SNAPSHOT"]]
