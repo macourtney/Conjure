dc2ab34bf88ee32c164fc5c4a55c303104d55151
Matt Courtney <macourtney@gmail.com>
2010-06-23 14:35:12 -0400

    Updated the leiningen dependency to 1.1.0 release.

--- lein_conjure/project.clj
+++ lein_conjure/project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [conjure-script "0.7.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [leiningen "1.1.0"]])
