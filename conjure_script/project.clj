250a4e7dcc94cd52c7bd96cf77122119d3c6d807
Matthew Courtney <mcourtne199@yahoo.com>
2010-07-17 06:55:52 -0400

    Updated the version to 0.7.0.

--- conjure_script/project.clj
+++ conjure_script/project.clj
@@ -1,6 +1,6 @@
-(defproject conjure-script "0.7.0-RC2"
+(defproject conjure-script "0.7.0"
   :description "Script libraries for Conjure. Conjure is a full stack web framework written entirely in Clojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [conjure-core "0.7.0-RC2"]]
+                 [conjure-core "0.7.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
