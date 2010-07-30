b1ab97ca3530e6c9662c25e124c30aa047adbf87
Matt Courtney <macourtney@gmail.com>
2010-07-30 12:47:43 -0400

    Added lazy loading of the ring adaptor to avoid compiling half of the files in Conjure (including test files).

--- conjure_core/src/conjure/core/server/servlet.clj
+++ conjure_core/src/conjure/core/server/servlet.clj
@@ -1,6 +1,18 @@
 (ns conjure.core.server.servlet
+  (:import [java.io FileNotFoundException])
   (:gen-class :extends javax.servlet.http.HttpServlet)
-  (:require [conjure.core.server.ring-adapter :as ring-adapter]
-            [ring.util.servlet :as ring-servlet]))
+  (:require [ring.util.servlet :as ring-servlet]))
 
-(ring-servlet/defservice ring-adapter/conjure)
+(defn
+  conjure-servlet [req]
+  (let [adaptor-namespace-symbol 'conjure.core.server.ring-adapter]
+    (try
+      (require adaptor-namespace-symbol)
+      (catch FileNotFoundException e))
+    (if-let [adaptor-namespace (find-ns adaptor-namespace-symbol)]
+      (if-let [adaptor-fn (ns-resolve adaptor-namespace 'conjure)]
+        (adaptor-fn req)
+        (println "Could not find the conjure ring adaptor function."))
+      (println "Could not find the conjure ring adaptor namespace.")))) 
+
+(ring-servlet/defservice conjure-servlet)
