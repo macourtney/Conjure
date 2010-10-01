07bc21a4ba4af5e73efd011b0d744c3fb1ea27a6
Matthew Courtney <mcourtne199@yahoo.com>
2010-09-30 21:28:51 -0400

    Updated conjure to RC2.

--- conjure/project.clj
+++ conjure/project.clj
@@ -1,9 +1,9 @@
-(defproject conjure "0.8.0-RC1"
+(defproject conjure "0.8.0-RC2"
   :description "Self extracting jar file for Conjure. Conjure is a full stack web framework written entirely in Clojure."
 
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [conjure-script "0.8.0-RC1"]]
+                 [conjure-script "0.8.0-RC2"]]
 
   :dev-dependencies [[lein-clojars "0.5.0"]]
 
