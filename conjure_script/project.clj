c4f407a607de93485d623737e742b07cdaee9d40
Matthew Courtney <mcourtne199@yahoo.com>
2010-10-25 12:32:46 -0400

    Updated the version numbers to 0.8.0.

--- conjure_script/project.clj
+++ conjure_script/project.clj
@@ -1,6 +1,6 @@
-(defproject conjure-script "0.8.0-RC2"
+(defproject conjure-script "0.8.0"
   :description "Script libraries for Conjure. Conjure is a full stack web framework written entirely in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [conjure-core "0.8.0-RC2"]]
+                 [conjure-core "0.8.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
--- lein_conjure/project.clj
+++ lein_conjure/project.clj
@@ -1,8 +1,8 @@
-(defproject lein-conjure "0.8.0-RC2"
+(defproject lein-conjure "0.8.0"
   :description "A leiningen plugin for Conjure. Conjure is a full stack web framework written entirely in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [conjure-script "0.8.0-RC2"]
+                 [conjure-script "0.8.0"]
                  [uk.org.alienscience/leiningen-war "0.0.7"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [leiningen "1.3.1"]])
