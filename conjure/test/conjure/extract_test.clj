cbaff506b28330bbc6398b559267fcf02f244cf0
Matt Courtney <macourtney@gmail.com>
2011-09-21 12:53:28 -0400

    Updated the version to 0.8.6-SNAPSHOT. Also removed the old conjure project which no one seems to be using.

--- conjure/test/conjure/extract_test.clj
+++
@@ -1,9 +0,0 @@
-(ns conjure.extract-test
-  (:import [java.io File])
-  (:use [conjure.extract] :reload-all)
-  (:use [clojure.test])
-  (:require [clojure.tools.file-utils :as file-utils]))
-
-(deftest test--main
-  (-main "test-project")
-  (file-utils/recursive-delete (File. "test_project")))
--- lein_conjure/project.clj
+++ lein_conjure/project.clj
@@ -1,5 +1,5 @@
-(defproject lein-conjure "0.8.5"
+(defproject lein-conjure "0.8.6-SNAPSHOT"
   :description "A leiningen plugin for Conjure. Conjure is a full stack web framework written entirely in Clojure."
-  :dependencies [[conjure-core "0.8.5"]
-                 [conjure-script "0.8.5"]]
+  :dependencies [[conjure-core "0.8.6-SNAPSHOT"]
+                 [conjure-script "0.8.6-SNAPSHOT"]]
   :eval-in-leiningen true)
