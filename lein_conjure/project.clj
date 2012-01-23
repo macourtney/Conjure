20ade97d09de65a370a7e75263863133c19302fb
Matt Courtney <macourtney@gmail.com>
2012-01-23 17:56:47 -0500

    Updated the version for conjure_core, conjure_script and lein_conjure to 0.8.8-SNAPSHOT.

--- lein_conjure/project.clj
+++ lein_conjure/project.clj
@@ -1,5 +1,5 @@
-(defproject lein-conjure "0.8.7"
+(defproject lein-conjure "0.8.8-SNAPSHOT"
   :description "A leiningen plugin for Conjure. Conjure is a full stack web framework written entirely in Clojure."
-  :dependencies [[conjure-core "0.8.7"]
-                 [conjure-script "0.8.7"]]
+  :dependencies [[conjure-core "0.8.8-SNAPSHOT"]
+                 [conjure-script "0.8.8-SNAPSHOT"]]
   :eval-in-leiningen true)
