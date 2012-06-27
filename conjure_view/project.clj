b8afd6c8c83794db3194c7aa326c8cfc157d4353
Matt Courtney <macourtney@gmail.com>
2012-06-27 14:07:56 -0400

    Changed conjure view to not be dependent on conjure model.

--- conjure_view/project.clj
+++ conjure_view/project.clj
@@ -5,8 +5,8 @@
                  [org.clojure/clojure "1.3.0"]
                  [org.conjure/conjure-config "0.9.0-SNAPSHOT"]
                  [org.conjure/conjure-html "0.9.0"]
-                 [org.conjure/conjure-model "0.9.0-SNAPSHOT"]
                  [org.conjure/conjure-util "0.9.0-SNAPSHOT"]
                  [scriptjure "0.1.24"]]
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
+                     [org.conjure/conjure-model "0.9.0-SNAPSHOT"]
                      [org.drift-db/drift-db-h2 "1.0.7"]])
