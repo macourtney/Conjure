a84547b64f64d079ce12b0bbcc64a373ef7bdc40
Matt Courtney <macourtney@gmail.com>
2012-07-11 15:04:48 -0400

    Added the conjure script view project.

---
+++ conjure_script_view/project.clj
@@ -0,0 +1,5 @@
+(defproject org.conjure/conjure-script-view "0.9.0-SNAPSHOT"
+  :description "The generator and destroyers for the view files."
+  :dependencies [[org.conjure/conjure-view "0.9.0-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]
+                     [org.conjure/conjure-model "0.9.0-SNAPSHOT"]])
