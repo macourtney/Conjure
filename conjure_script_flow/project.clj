4ce6fa3a84333abcfc60e0538fdea204c128750c
Matt Courtney <macourtney@gmail.com>
2012-07-17 17:02:32 -0400

    Updated the project file in conjure script flow to say flow instead of view.

--- conjure_script_flow/project.clj
+++ conjure_script_flow/project.clj
@@ -1,5 +1,5 @@
-(defproject org.conjure/conjure-script-view "0.9.0-SNAPSHOT"
-  :description "The generator and destroyers for the view files."
+(defproject org.conjure/conjure-script-flow "0.9.0-SNAPSHOT"
+  :description "The generator and destroyers for the flow files."
   :dependencies [[org.conjure/conjure-flow "0.9.0-SNAPSHOT"]
                  [org.conjure/conjure-test "0.9.0-SNAPSHOT"]]
   :dev-dependencies [[org.clojure/clojure "1.2.1"]])
