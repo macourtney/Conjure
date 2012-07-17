f03086e0c857d88d9abc0300dc46f61a5d54b615
Matt Courtney <macourtney@gmail.com>
2012-07-17 17:01:49 -0400

    Added the conjure script plugin project.

---
+++ conjure_script_plugin/project.clj
@@ -0,0 +1,5 @@
+(defproject org.conjure/conjure-script-plugin "0.9.0-SNAPSHOT"
+  :description "The generator and destroyers for the plugin files."
+  :dependencies [[org.conjure/conjure-plugin "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-test "0.9.0-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]])
