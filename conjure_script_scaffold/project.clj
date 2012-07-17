65dfa36593e3bfd95fb0bff1ba19539fd51623fd
Matt Courtney <macourtney@gmail.com>
2012-07-17 17:15:40 -0400

    Added the conjure script scaffold project.

---
+++ conjure_script_scaffold/project.clj
@@ -0,0 +1,9 @@
+(defproject org.conjure/conjure-script-scaffold "0.9.0-SNAPSHOT"
+  :description "The generator and destroyers for the scaffold files."
+  :dependencies [[org.conjure/conjure-model "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-flow "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-model "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-view "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-test "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-view "0.9.0-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]])
