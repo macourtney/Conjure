2c607100e91b2f942b07704bcfb3a0bd15a74229
Matt Courtney <macourtney@gmail.com>
2012-07-18 14:22:35 -0400

    Added the conjure script full project.

---
+++ conjure_script_full/project.clj
@@ -0,0 +1,8 @@
+(defproject org.conjure/conjure-script-full "0.9.0-SNAPSHOT"
+  :description "Include this project to include all of the conjure script project jars at once."
+  :dependencies [[org.conjure/conjure-script-core "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-flow "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-model "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-plugin "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-scaffold "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-view "0.9.0-SNAPSHOT"]])
