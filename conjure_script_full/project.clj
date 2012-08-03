586fd68a727a08e47acd038eefbf05bf8a33dd7f
Matt Courtney <macourtney@gmail.com>
2012-08-03 15:11:36 -0400

    Removed the unneeded conjure script full project.

--- conjure_script_full/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject org.conjure/conjure-script-full "0.9.0-SNAPSHOT"
-  :description "Include this project to include all of the conjure script project jars at once."
-  :dependencies [[org.conjure/conjure-script-core "0.9.0-SNAPSHOT"]
-                 [org.conjure/conjure-script-plugin "0.9.0-SNAPSHOT"]
-                 [org.conjure/conjure-script-scaffold "0.9.0-SNAPSHOT"]])
