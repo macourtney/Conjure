12b87a453d8b1a21752c4731f7545123b37997a3
Matt Courtney <macourtney@gmail.com>
2012-09-07 17:31:14 -0400

    Moved the leiningen plugin file into conjure script core to allow people to only include the conjure plugins they want.

--- lein_conjure/project.clj
+++ lein_conjure/project.clj
@@ -1,6 +1,7 @@
 (defproject org.conjure/lein-conjure "0.9.0-SNAPSHOT"
   :description "A leiningen plugin for Conjure. Conjure is a full stack web framework written entirely in Clojure."
-  :dependencies [[org.conjure/conjure-script-plugin "0.9.0-SNAPSHOT"]
+  :dependencies [[org.conjure/conjure-script-core "0.9.0-SNAPSHOT"]
+                 [org.conjure/conjure-script-plugin "0.9.0-SNAPSHOT"]
                  [org.conjure/conjure-script-scaffold "0.9.0-SNAPSHOT"]
                  [org.conjure/conjure-util "0.9.0-SNAPSHOT"]]
 
